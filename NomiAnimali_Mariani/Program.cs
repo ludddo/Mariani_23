626559d1be809b81450f0634d1cd226c153bb672
Ludovico Mariani <mariani.ludovico.studente@itispaleocapa.it>
2023-02-01 13:12:59 +0100

    Aggiungere i file di progetto.

---
+++ NomiAnimali_Mariani/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomiAnimali_Mariani
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
